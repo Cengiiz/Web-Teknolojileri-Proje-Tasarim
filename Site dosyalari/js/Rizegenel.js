8a0267c6ef377f95530b96c6f114f47a7d38712c
Cengiiz <dadem64@gmail.com>
2021-05-09 18:29:57 +0300

    Create Rizegenel.js

---
+++ Site dosyalari/js/Rizegenel.js
@@ -0,0 +1,9 @@
+/*Gizleme kodu*/
+function hide(id){
+		var divObject=document.getElementById(id);
+		divObject.style.display="none";
+	}
+function show(id){
+		var divObject=document.getElementById(id);
+		divObject.style.display="block";
+	}
