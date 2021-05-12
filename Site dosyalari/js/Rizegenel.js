7c58a1e7c9be42de61f65649c22f079e6bf3d50f
Cengiiz <dadem64@gmail.com>
2021-05-12 15:11:10 +0300

    Delete Rizegenel.js

--- Site dosyalari/js/Rizegenel.js
+++
@@ -1,9 +0,0 @@
-/*Gizleme kodu*/
-function hide(id){
-		var divObject=document.getElementById(id);
-		divObject.style.display="none";
-	}
-function show(id){
-		var divObject=document.getElementById(id);
-		divObject.style.display="block";
-	}
